598fe5152be4e24707699dca2c4ea7a93a9045ce
jstanley14 <jstanley@middil.com>
2014-09-12 08:32:29 -0400

    SPIK-235 - admin controller

---
+++ shell/app/controllers/Admin.scala
@@ -0,0 +1,28 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Admin extends Controller {
+//
+//  def main = Action {
+//    Ok(views.html.main("CAP Login"))
+//  }
+
+  def milCS(userName: String) = Action {
+    Ok(views.html.mil_cs(userName))
+  }
+
+  def orgAdmin(userName: String) = Action {
+    Ok(views.html.org_admin(userName))
+  }
+
+  def teacher(userName: String) = Action {
+    Ok(views.html.teacher(userName))
+  }
+
+  def student(userName: String) = Action {
+    Ok(views.html.student(userName))
+  }
+
+}
--- shell/app/controllers/Application.scala
+++ shell/app/controllers/Application.scala
@@ -1,28 +1,10 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
+import play.api.mvc.{Controller, Action}
 
 object Application extends Controller {
 
   def main = Action {
     Ok(views.html.main("CAP Login"))
   }
-
-  def milCS(userName: String) = Action {
-    Ok(views.html.mil_cs(userName))
-  }
-
-  def orgAdmin(userName: String) = Action {
-    Ok(views.html.org_admin(userName))
-  }
-
-  def teacher(userName: String) = Action {
-    Ok(views.html.teacher(userName))
-  }
-
-  def student(userName: String) = Action {
-    Ok(views.html.student(userName))
-  }
-
 }
